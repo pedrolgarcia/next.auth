9ab564b20691f748e34db9d47a5f058ea5e8a537
Pedro Lopes <pedrolopesgrc@gmail.com>
2022-11-19 16:16:16 -0300

    feat: token on setcookie with nookies

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,5 +1,7 @@
-import { createContext, ReactNode, useState } from 'react'
+import { createContext, ReactNode, useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
+import { parseCookies, setCookie } from 'nookies'
+
 import { api } from '../services/api'
 
 type User = {
@@ -32,6 +34,17 @@ export function AuthProvider({ children }: AuthProviderProps) {
 
   const isAuthenticated = !!user.email
 
+  useEffect(() => {
+    const { '@next.auth:token': token } = parseCookies()
+
+    if (token) {
+      api.get('/me').then((response) => {
+        const { email, permissions, roles } = response.data
+        setUser({ email, permissions, roles })
+      })
+    }
+  }, [])
+
   async function signIn({ email, password }: SignInCredentials) {
     try {
       const response = await api.post('sessions', {
@@ -39,13 +52,29 @@ export function AuthProvider({ children }: AuthProviderProps) {
         password,
       })
 
-      const { permissions, roles } = response.data
+      const { token, refreshToken, permissions, roles } = response.data
+
+      // sessionStorage - ao fechar o app (navegador) ele morre
+      // localStorage - é permanente mas não temos acesso no server side
+      // cookies
+
+      setCookie(undefined, '@next.auth:token', token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/',
+      })
+
+      setCookie(undefined, '@next.auth:refreshToken', refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/',
+      })
 
       setUser({
         email,
         permissions,
         roles,
       })
+
+      api.defaults.headers['Authorization'] = `Bearer ${token}`
 
       router.push('/dashboard')
     } catch (error) {
